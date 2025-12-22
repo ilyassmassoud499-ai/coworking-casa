634b08d1b97bb7d4f01a030f70f542cc1e4d15df
ilyass <ilyassmassoud499@gmail.com>
2025-12-22 12:35:38 +0100

    modi

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,5 +1,10 @@
 
 import { useSelector } from "react-redux";
+import revenuJ from "../assets/revenuJ.jpg"
+import revenuT from "../assets/revenuT.jpg"
+import bureau from "../assets/bureau-privee.jpg"
+import reservation from "../assets/reservation.jpg"
+
 
 const espaces = {
   "Bureau privé": 200,
@@ -22,30 +27,43 @@ function Dashboard() {
   const espacePlusReserve = Object.entries(tauxOccupation).sort((a,b)=>parseInt(b[1])-parseInt(a[1]))[0]?.[0] || "Aucun";
 
   return (
-    <div className="container mt-5">
+    <div  className="container mt-5">
       <h2>Dashboard</h2>
-      <div className="row">
-        <div className="col-md-4 mb-3">
-          <div className="card text-white bg-success">
+      <div className="row g-4 ">
+        <div className="col-md-3 mb-3">
+          <div className="card shadow-sm h-100 border-0 ">
+            <img src={revenuJ}  className="card-img-top"     />
             <div className="card-body">
               <h5 className="card-title">Revenu du jour</h5>
               <p className="card-text">{revenuJour} DH</p>
             </div>
           </div>
         </div>
-        <div className="col-md-4 mb-3">
-          <div className="card text-white bg-info">
+        <div className="col-md-3 mb-3">
+          <div className="card shadow-sm h-100 border-0   ">
+             <img src={revenuT}  className="card-img-top"     />
             <div className="card-body">
               <h5 className="card-title">Revenu total</h5>
               <p className="card-text">{revenuMois} DH</p>
             </div>
           </div>
         </div>
-        <div className="col-md-4 mb-3">
-          <div className="card text-white bg-warning">
+        <div className="col-md-3 mb-3">
+          <div className="card shadow-sm h-100 border-0  ">
+            <img src={bureau} className="card-img-top"  />
             <div className="card-body">
               <h5 className="card-title">Espace le plus réservé</h5>
               <p className="card-text">{espacePlusReserve}</p>
+            </div>
+          </div>
+        </div>
+             <div className="col-md-3 mb-3">
+          <div className="card shadow-sm h-100 border-0  ">
+            <img src={reservation} className="card-img-top"  />
+            <div className="card-body">
+             <h5 className="card-title">Nombre total de réservations </h5>
+             <p className="card-text">{reservations.length}</p>
+              
             </div>
           </div>
         </div>
@@ -60,7 +78,7 @@ function Dashboard() {
           </li>
         ))}
       </ul>
-      <h4 className="mt-4">Nombre total de réservations : {reservations.length}</h4>
+      
     </div>
   );
 }
